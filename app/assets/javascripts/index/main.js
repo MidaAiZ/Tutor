945492194488e2fb07e204555838de1e286f37a3
Wu Mingda <1036570699@qq.com>
2016-11-11 01:49:38 +0800

    初步构建前台页面

---
+++ app/assets/javascripts/index/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#condition-header-1').click(function(event) {
+        event.stopPropagation();
+        $('#condition-menu-1').slideToggle(100, 'linear', function() {
+            $(this).attr('expand') === 'false' ? $(this).attr('expand', 'ture') : $(this).attr('expand', 'false');
+        });
+    });
+});
