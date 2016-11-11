575d0c54c98c581c7c418f1d849d52e80141edfb
Wu Mingda <1036570699@qq.com>
2016-11-11 21:21:02 +0800

    添加轮拨图

--- app/assets/javascripts/index/main.js
+++ app/assets/javascripts/index/main.js
@@ -1,8 +1,13 @@
+// = require bootstrap/carousel
+
 $(document).ready(function() {
     $('#condition-header-1').click(function(event) {
         event.stopPropagation();
         $('#condition-menu-1').slideToggle(100, 'linear', function() {
             $(this).attr('expand') === 'false' ? $(this).attr('expand', 'ture') : $(this).attr('expand', 'false');
         });
+    });
+    $('.carousel').carousel({
+        interval: 2000
     });
 });
