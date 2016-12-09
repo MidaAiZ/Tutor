0da7373da4818f3c3751247197c493e52857fbb9
Wu Mingda <1036570699@qq.com>
2016-12-09 17:08:52 +0800

    实现flowShow

--- app/assets/javascripts/index/main.js
+++ app/assets/javascripts/index/main.js
@@ -10,4 +10,64 @@ $(document).ready(function() {
     $('.carousel').carousel({
         interval: 2000
     });
+
+
+    setLabelFlow($("li[role='flow-label']"));
+
 });
+
+function setLabelFlow($flowEle) {
+    var flowParams = {
+        width: $flowEle.parent().width(),
+        height: $flowEle.parent().height(),
+        length: $flowEle.length,
+    };
+
+    //使每一个元素进入循环
+    for (var i = 0; i < flowParams.length; i++) {
+        setTimeout(function(index) {
+            return function() {
+                showFlow($flowEle.slice(index, index + 1));
+            }
+        }(i), 1000 * setRandom());
+    }
+
+    //根据元素个数返回一个随机数
+    function setRandom() {
+        return Math.round(Math.random() * flowParams.length)
+    }
+
+    //设置离父级元素左边的距离
+    function setFlowPositionX($ele) {
+        return privateWidth = Math.round(Math.random() * (flowParams.width - $ele.width()));
+    }
+
+    //设置上升速度
+    function setSpeed() {
+        return (Math.random() + 1) * (flowParams.height * 10)
+    }
+
+    function showFlow($ele) {
+        $ele.finish(true);
+        var flowSpeed = setSpeed();
+        $ele.animate({
+            left: setFlowPositionX($ele),
+            marginLeft: 0
+        }, 0).animate({
+            opacity: 'show',
+            top: '80%'
+        }, flowSpeed * 0.2, "linear").animate({
+            top: '20%'
+        }, flowSpeed * 0.6, "linear").animate({
+            opacity: 'hide',
+            top: 0
+        }, flowSpeed * 0.2, "linear").animate({
+            opacity: 'hide',
+            top: '100%',
+            left: 0
+        }, 0);
+        setTimeout(function() {
+            showFlow($ele)
+        }, 1000 * setRandom() + flowSpeed)
+    }
+}
