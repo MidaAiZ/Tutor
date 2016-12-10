6ea14ca0bc309bbd89c89bc0af9b053ee4bfba03
Wu Mingda <1036570699@qq.com>
2016-12-10 19:04:05 +0800

    继续完善评价功能，基本完成，修复一些bug

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,6 +10,7 @@ class CommentsController < ApplicationController
   # GET /comments/1
   # GET /comments/1.json
   def show
+      @course = @comment.course
   end
 
   # GET /comments/new
@@ -53,16 +54,6 @@ class CommentsController < ApplicationController
       end
     else
        redirect_to ucenter_course_path(@course), notice: '修改失败！.'
-    end
-  end
-
-  # DELETE /comments/1
-  # DELETE /comments/1.json
-  def destroy
-    @comment.destroy
-    respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
